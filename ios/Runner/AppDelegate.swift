af0e50ec730442fd37ffd76d984b4f1018896342
eucalvo <eliamcalvo24@gmail.com>
2023-06-24 15:43:13 -0600

    change ios to get google maps api key from .env file

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import dotenv
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,8 +10,13 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    let apiKey = ProcessInfo.processInfo.environment["GOOGLE_MAPS_API_KEY_IOS"]
+
+    let dotenv = Dotenv()
+    dotenv.load()
+
+    let apiKey = dotenv["GOOGLE_MAPS_API_KEY_IOS"]
     GMSServices.provideAPIKey(apiKey!)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
